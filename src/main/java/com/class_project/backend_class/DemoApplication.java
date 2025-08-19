e0af28de53b51ac39e750dcdb6d289f81ffa78d9
vazvdr <azevedo.vanderson@hotmail.com>
2025-08-19 12:50:43 -0300

    Removing dotenv dependency for deploy

--- src/main/java/com/class_project/backend_class/DemoApplication.java
+++ src/main/java/com/class_project/backend_class/DemoApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @EnableCaching
 @SpringBootApplication
 public class DemoApplication {
