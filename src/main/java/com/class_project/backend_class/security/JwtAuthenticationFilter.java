e5ac863617f044dd1ed1e183f596be7a0d51ddb7
vazvdr <azevedo.vanderson@hotmail.com>
2025-12-11 16:16:11 -0300

    Tirando o filter chain para requisicoes options

--- src/main/java/com/class_project/backend_class/security/JwtAuthenticationFilter.java
+++ src/main/java/com/class_project/backend_class/security/JwtAuthenticationFilter.java
@@ -37,6 +37,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+    	
+    	if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+    	    response.setStatus(HttpServletResponse.SC_OK);
+    	    return;
+    	}
 
         String authHeader = request.getHeader("Authorization");
 
